a250058462deaf08593f8562b1621bc5edb4490b
nickfoden <nickfoden@gmail.com>
2019-01-02 11:02:30 -0500

    Changing to be import default

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-module.exports = function tiny(string) {
+// module.exports = function tiny(string) {
+//   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+//   return string.replace(/\s/g, "");
+// };
+
+const tiny = string => {
   if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
   return string.replace(/\s/g, "");
 };
+
+export default tiny;
